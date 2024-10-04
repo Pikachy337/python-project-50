4f6751e942558b30d74d3dc69cf2a72c7092befe
Pikachy <pikachyshkaa@gmail.com>
2024-10-04 17:52:58 +0500

    add open logic JSON format

--- gendiff/code/gendiff_main.py
+++ gendiff/code/gendiff_main.py
@@ -1,5 +1,5 @@
 import argparse
-
+import json
 
 def about_gendiff():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
@@ -7,10 +7,13 @@ def about_gendiff():
     # Positional arguments
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-
     # Optional argument
     parser.add_argument('-f', '--format', type=str, help='set format of output')
 
     args = parser.parse_args()
+
+    with open(args.first_file) as f1, open(args.second_file) as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
 
     return args
