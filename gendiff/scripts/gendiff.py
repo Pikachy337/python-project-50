9f5ae81b3be62bab594f096cc07cf257a5034fe8
Dmitry <pikachyshkaa@gmail.com>
2024-11-28 21:10:10 +0500

    removing unused modules/functions

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,3 @@
-from gendiff.diff_generator import generate_diff
 from gendiff.cmd_interface import cli
 
 
@@ -6,11 +5,5 @@ def main():
     cli()
 
 
-def diff():
-    print(generate_diff('../../tests/fixtures/file1.json',
-                        '../../tests/fixtures/file2.json'))
-
-
 if __name__ == '__main__':
-    diff()
     main()
